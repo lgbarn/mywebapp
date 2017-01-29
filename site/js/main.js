9b436a44f9fbd0ab5fd06f774da1ecfc144a943e
Luther Barnum <luther.barnum@gmail.com>
2017-01-29 15:30:08 -0500

    changed named in all files to match index.html

--- site/js/main.js
+++ site/js/main.js
@@ -104,14 +104,14 @@ $(".link-home").on('click', function(event){
   }  
 });
 
-//==============___Blog - Ajax___================
+//==============___chess - Ajax___================
 function loadPost(){
    $.ajax({
       url: 'single.html', // URL HERE
       type: 'GET',
       success: function(html) {
 
-        var $lis = $(html).find('#blogPost'); // Loads the content inside #blogPost div
+        var $lis = $(html).find('#chessPost'); // Loads the content inside #chessPost div
 
         $("#postHere").html($lis);
     }
@@ -122,7 +122,7 @@ $(".loadPost").on('click', function(event){
   event.preventDefault();
   //$("#postHere").html('loading...');
   $('.section-page-active').removeClass('section-page-active');
-  $('#page-blog-single').addClass('section-page-active');
+  $('#page-chess-single').addClass('section-page-active');
   pageOn();
   loadPost();
 });
